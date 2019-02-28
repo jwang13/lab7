ea3b96eb796a59a1261c5344c472ae1b78f9a5f4
jwang13 <jennywang01@college.harvard.edu>
2019-02-28 14:07:29 -0500

    1 and 2

--- lab7_part1.ml
+++ lab7_part1.ml
@@ -58,12 +58,14 @@ temporary value pending your putting in appropriate ones.)
 
 module Math : MATH =
   struct
-    let pi = nan
-    let cos _ = nan
-    let sin _ = nan
-    let sum _ _ = nan
-    let max _ = None
-  end ;;
+    let pi = 4.0 *. atan 1.0
+    let cos = cos 
+    let sin = sin 
+    let sum = (+.)
+    let max inlist = if inlist = [] then None else
+      Some (List.fold_left (fun a b -> if a > b then a else b) (List.hd inlist) inlist)
+  end
+  ;;
 
 (*......................................................................
 Exercise 1B: Now that you've implemented the Math module, use it to
@@ -72,7 +74,7 @@ type float option. Name the resulting value "result". (Do not use a
 local open for this exercise.)
 ......................................................................*)
 
-let result = Some nan ;;
+let result = Math.max [(Math.cos Math.pi);(Math.sin Math.pi)] ;;
 
 (*......................................................................
 Exercise 1C: Reimplement the computation from 1B above, now as
--- lab7_part2.ml
+++ lab7_part2.ml
@@ -71,7 +71,7 @@ Exercise 2A: Extract the red channel of the color named Red, naming
 the result "red_channel".
 ......................................................................*)
 
-let red_channel : int = 0 ;;
+let red_channel = Color.color_named Red;;
 
 (* We hope you'll find the module system quite useful, once you get
 the hang of the conventions.
